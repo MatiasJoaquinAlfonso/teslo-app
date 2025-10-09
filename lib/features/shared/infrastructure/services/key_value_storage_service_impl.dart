9a1b5ade8f34ce34ed35f749893139c449c80f4e
MatiasJoaquinAlfonso <joaquinmatias332@gmail.com>
2025-10-09 15:03:11 -0300

    implementacion completa del shared preferences

--- lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
+++ lib/features/shared/infrastructure/services/key_value_storage_service_impl.dart
@@ -1,26 +1,57 @@
-import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
-
+import 'key_value_storage_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class KeyValueStorageServiceImpl extends KeyValueStorageService {
-  
-  
-  @override
-  Future<T?> getValue<T>(String key) {
-    // TODO: implement getValue
-    throw UnimplementedError();
+
+  Future<SharedPreferences> getSharedPrefs() async {
+    return await SharedPreferences.getInstance();
   }
 
   @override
-  Future<bool> removeKey(String key) {
-    // TODO: implement removeKey
-    throw UnimplementedError();
+  Future<T?> getValue<T>(String key) async {
+
+    final prefs = await getSharedPrefs();
+
+    switch (T) {
+      case int:
+        return prefs.getInt(key) as T?;
+
+      case String:
+        return prefs.getString(key) as T?;
+      
+      default:
+        throw UnimplementedError('No esta implementado para un tipo ${T.runtimeType}');
+    
+    }
+
   }
 
   @override
-  Future<void> setKeyValue<T>(String key, T value) {
-    // TODO: implement setKeyValue
-    throw UnimplementedError();
+  Future<bool> removeKey(String key) async {
+    final prefs = await getSharedPrefs();
+    return prefs.remove(key);
   }
 
+  @override
+  Future<void> setKeyValue<T>(String key, T value) async {
+
+    final prefs = await getSharedPrefs();
+
+    switch (T) {
+      case int:
+        prefs.setInt(key, value as int);
+        break;
+
+      case String:
+        prefs.setString(key, value as String);
+        break;
+      
+      default:
+        throw UnimplementedError('No esta implementado para un tipo ${T.runtimeType}');
+    
+    }
+
+
+  }
 
 }
