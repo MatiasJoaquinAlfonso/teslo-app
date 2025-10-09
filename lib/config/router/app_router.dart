fe5934be90061c1ac32e4de61b3e7fbe26c4492e
MatiasJoaquinAlfonso <joaquinmatias332@gmail.com>
2025-10-09 15:56:06 -0300

    implementacion con provider para el Go router, de esta forma validar el la authenticaciond el usuario al movernos entre pantallas

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,32 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
+final goRouterProvider = Provider((ref) {
 
-    GoRoute(
-      path: '/splash',
-      builder: (context, state) => const CheckAuthStatusScreen(),
-    ),    
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+      ///* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
 
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+    ///! TODO: Bloquear si no se está autenticado de alguna manera
+  );
+});
--- lib/features/auth/presentation/screens/check_auth_status_screen.dart
+++ lib/features/auth/presentation/screens/check_auth_status_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class CheckAuthStatusScreen extends StatelessWidget {
   const CheckAuthStatusScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
 
-    
     return Scaffold(
       body: Center(
         child: CircularProgressIndicator(strokeWidth: 2,),
