755c8c9a5e60dabd0b8bd4b5b9e07c3c7f2b7fb6
MatiasJoaquinAlfonso <joaquinmatias332@gmail.com>
2025-10-10 23:21:23 -0300

    implementacion de la cama de domain y infraestructure para crear los datasources y los repositories

---
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsDatasourceImpl extends ProductsDatasource {
+
+
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
+    // TODO: implement createUpdateProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Product> getProductsById(String id) {
+    // TODO: implement getProductsById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Product> getProductsByPage({int limit = 10, int offset = 0}) {
+    // TODO: implement getProductsByPage
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Product>> searchProductsByTerms(String term) {
+    // TODO: implement searchProductsByTerms
+    throw UnimplementedError();
+  }
+
+}
